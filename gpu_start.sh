44296b0278a762352182da08b8ac19f461dd6b74
ajhamdi <ajhamdi@yahoo.com>
2019-03-21 08:27:50 +0300

    environment

--- gpu_start.sh
+++ gpu_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH --job-name resnet
-#SBATCH --array=0
+#SBATCH --array=0-9
 #SBATCH --reservation=IVUL
 #SBATCH --time=24:00:00
 #SBATCH --gres=gpu:1
@@ -21,7 +21,7 @@ echo $DIR
 echo `pwd`
 echo `hostname`
 
-python main.py --network=resnet --all_points=0 --class_nb=1 --object_nb=1 --iterations=5 --override=1 --reduced=0
+python main.py --network=resnet --all_points=1 --class_nb=${SLURM_ARRAY_TASK_ID} --object_nb=1 --iterations=600 --override=1 --reduced=0
 #python main.py --exp_type="GP" --class_nb=${SLURM_ARRAY_TASK_ID} --gaussian_nb=1 --scenario_nb=0  --is_randomize=False --dataset_nb=2 --evolution_nb=1 --task_nb=${SLURM_ARRAY_TASK_ID} --valid_size=250 --log_frq=1000 --batch_size=64 --K=10 --induced_size=1000 --retained_size=2500 --ind_frq=4 --nb_steps=400 --gendist_size=20000 --is_train=False --is_gendist=False --is_cluster=True --is_visualize=False --cont_train=False --optimize_oracle=False --restore_all=False --is_focal=False --is_evolve=False --keep_bank=False --full_set=True --learning_rate_t=0.0001 --learning_rate_g=0.0001 > /dev/null
 #python main.py --exp_type="Gaussian" --class_nb=${SLURM_ARRAY_TASK_ID} --gaussian_nb=1000 --scenario_nb=0  --is_randomize=False --dataset_nb=2 --evolution_nb=1 --task_nb=${SLURM_ARRAY_TASK_ID} --valid_size=250 --log_frq=1000 --batch_size=64 --K=10 --induced_size=1000 --retained_size=2500 --ind_frq=4 --nb_steps=400 --gendist_size=20000 --is_train=True --is_gendist=False --is_cluster=True --is_visualize=False --cont_train=False --optimize_oracle=False --restore_all=False --is_focal=False --is_evolve=False --keep_bank=False --full_set=True --learning_rate_t=0.0001 --learning_rate_g=0.0001 > /dev/null
 #python main.py --exp_type="Adversarial" --class_nb=${SLURM_ARRAY_TASK_ID} --scenario_nb=0  --is_randomize=True --dataset_nb=2 --evolution_nb=1 --task_nb=${SLURM_ARRAY_TASK_ID} --valid_size=250 --log_frq=1000 --batch_size=128 --K=10 --induced_size=1000 --retained_size=5000 --ind_frq=4 --nb_steps=400 --gendist_size=1000 --is_train=True --is_gendist=False --is_cluster=True --is_visualize=False --cont_train=False --optimize_oracle=False --restore_all=False --is_focal=False --is_evolve=True --keep_bank=False --full_set=True  > /dev/null
@@ -32,4 +32,4 @@ python main.py --network=resnet --all_points=0 --class_nb=1 --object_nb=1 --iter
 #python main.py --dataset_nb=5 --exp_no=55 --valid_size=20 --bb_log_frq=40 --batch_size=64 --K=10 --induced_size=500 --bb_ind_frq=10000 --nb_steps=600 --gendist_size=3000 --is_train=True --is_gendist=False --is_cluster=True --cont_train=False --optimize_oracle=False --restore_all=False --is_focal=False --is_evolve=False --keep_bank=False --full_set=True --learning_rate_t=0.0001 --learning_rate_g=0.0001 
 #python main.py --dataset_nb=5 --exp_no=56 --valid_size=20 --bb_log_frq=40 --batch_size=64 --K=10 --induced_size=500 --bb_ind_frq=10000 --nb_steps=600 --gendist_size=3000 --is_train=True --is_gendist=False --is_cluster=True --cont_train=False --optimize_oracle=False --restore_all=False --is_focal=False --is_evolve=False --keep_bank=False --full_set=True --learning_rate_t=0.0001 --learning_rate_g=0.0001 
 
-rm core.*
+# rm core.*
