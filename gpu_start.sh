1a417a2644f81102ac54937a5ba01cbad95bdd8f
Abdullah J. Hamdi <hamdiaj@dgpu105-09.ibex.kaust.edu.sa>
2019-03-28 12:50:23 +0300

    supplementary

--- gpu_start.sh
+++ gpu_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #SBATCH --job-name inc99
-#SBATCH --array=0
+#SBATCH --array=0-9
 #SBATCH --reservation=IVUL
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --gres=gpu:1
 #SBATCH -o logs/output.%a.%j.out
 #SBATCH -e logs/output.%a.%j.err
@@ -23,7 +23,7 @@ echo `hostname`
 
 # python main.py --network=resnet --all_points=1 --class_nb=${SLURM_ARRAY_TASK_ID} --object_nb=1 --iterations=600 --override=1 --reduced=0
 # python main.py --network=incept --all_points=1 --class_nb=9 --object_nb=1 --iterations=600 --override=1 --reduced=0 --custom_points=1 --custom_list=${SLURM_ARRAY_TASK_ID}
-python map.py --all_shapes=0 --class_nb=${SLURM_ARRAY_TASK_ID} --object_nb=0 --precisions=3 --override=1 --custom_shapes=0
+python map.py --all_shapes=1 --class_nb=${SLURM_ARRAY_TASK_ID} --object_nb=0 --precisions=3 --override=1 --custom_shapes=0
 
 
 
